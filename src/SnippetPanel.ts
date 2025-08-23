902115e79cdd6c4dfbe6e7fa260c66d16e320621
Randolph Pierre <randolph.mpierre@gmail.com>
2025-08-23 18:01:19 -0400

    Initial commit - added src and extension files

---
+++ src/SnippetPanel.ts
@@ -0,0 +1,8 @@
+import * as vscode from 'vscode';
+
+export class SnippetPanel {
+  public static createOrShow(extensionUri: vscode.Uri) {
+    console.log('SnippetPanel created');
+    // Webview logic to be added later
+  }
+}
